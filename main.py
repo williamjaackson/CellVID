e81fce3cfc548d0979878b43fdc24637a504324d
itskegnh <64617857+itskegnh@users.noreply.github.com>
2021-08-14 14:42:54 +1000

    fix size unknown

--- main.py
+++ main.py
@@ -30,10 +30,9 @@ class CellVid:
       #  break
       count += 1
   
-  def render(self):
+  def render(self, size=128):
     for i, frame in enumerate(os.listdir(self.frames_path)):
-
-      print(self.frames_path)      
+      
       frame_image = Image.open(self.frames_path + f"frame{i}.png")
       frame_image = frame_image.resize((int(size * frame_image.width / frame_image.height), size), Image.ANTIALIAS)
 
