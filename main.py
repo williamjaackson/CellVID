81bd2e3548cf7c7f79127d14c13de3a1ae683142
itskegnh <64617857+itskegnh@users.noreply.github.com>
2021-08-15 12:44:47 +1000

    up

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     video_path = sys.argv[1]
     frame_rate = int(sys.argv[2])
     frame_steps = int(sys.argv[3])
-    frame_count = int(sys.argv[4])-1
     size = int(sys.argv[5])
+    frame_count = int(sys.argv[4])-1
     step = sys.argv[6]
   cellvid = CellVid(video_path, frame_rate, frame_steps, frame_count, size, step)
